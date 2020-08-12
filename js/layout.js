5956fe4904233c7988db69e3875b10b5dc3d663f
S4NGS <dessign.sangs@gmail.com>
2020-08-12 16:51:05 +0900

    update

--- js/layout.js
+++ js/layout.js
@@ -4,40 +4,4 @@ var gnb_footer = document.querySelector('#gnb_footer');
 document.body.style.paddingTop = header.offsetHeight + 'px';
 document.body.style.paddingBottom = gnb_footer.offsetHeight + 'px';
 
-var mySwiper = new Swiper('.swiper-container', {
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    }
-});
-
 $('.section').addClass('transition');
-//new fullpage('#fullpage', {
-//    licenseKey: '0EA38F38-7D9D4210-8AFFEEAD-29BEAE4D',
-//    slidesNavigation: true,
-//    slidesNavPosition: 'bottom',
-//    fadingEffect: true,
-//    scrollOverflow: true,
-//    scrollOverflowReset: true,
-//    touchSensitivity: 0,
-//    paddingTop: '58px',
-//    paddingBottom: '54px',
-//    normalScrollElements: '#sec1, #sec2',
-//    onLeave: function (origin, destination, direction) {
-//        var loadedSection = this;
-//        if (destination.index == 2) {
-//            logo('lays');
-//        }else if (destination.index == 3) {
-//            logo('lace');
-//        }else if(destination.index == 1 && direction == 'up'){
-//            logo('lace');
-//        }
-//        
-//    },afterRender: function(){
-//        var pluginContainer = this;
-//    }
-//});
